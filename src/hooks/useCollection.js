5fedbe075d5ad5875d44356d17942400877cfdb9
drogan xp <goodmsaul@gmail.com>
2022-08-01 19:59:50 +0545

    final commit project completion

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, useRef } from 'react'
+
+// firebase imports
+import { db } from '../firebase/config'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
+
+export const useCollection = (c, _q) => {
+    const [documents, setDocuments] = useState(null)
+
+    const q = useRef(_q).current
+
+    useEffect(() => {
+        let ref = collection(db, c)
+        if (q) ref = query(ref, where(...q))
+
+        const unsub = onSnapshot(ref, (snapshot) => {
+            let results = []
+            snapshot.docs.forEach((doc) => {
+                results.push({ id: doc.id, ...doc.data() })
+            })
+            setDocuments(results)
+        })
+
+        return () => unsub()
+    }, [c, q])
+
+    return { documents }
+}
